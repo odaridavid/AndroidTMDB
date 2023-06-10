9573788eaabbc7cca0a9ae0ff6f042f89c9c9134
odaridavid <davidkibzodari@gmail.com>
2023-06-10 15:00:24 +0200

    add usecase test blueprint

---
+++ app/src/test/java/dev/davidodari/androidtmdb/usecases/GetLatestMovieDetailsUseCaseTest.kt
@@ -0,0 +1,5 @@
+package dev.davidodari.androidtmdb.usecases
+
+class GetLatestMovieDetailsUseCaseTest {
+
+}
---
+++ app/src/test/java/dev/davidodari/androidtmdb/usecases/GetLatestMoviesListUseCaseTest.kt
@@ -0,0 +1,4 @@
+package dev.davidodari.androidtmdb.usecases
+
+class GetLatestMoviesListUseCaseTest {
+}
