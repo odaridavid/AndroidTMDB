d7f883f4c5e5178d4df69ee0dba8c20d4a0549ea
odaridavid <davidkibzodari@gmail.com>
2023-06-10 15:34:45 +0200

    setup nav controller

--- app/src/main/java/dev/davidodari/androidtmdb/MainActivity.kt
+++ app/src/main/java/dev/davidodari/androidtmdb/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import dev.davidodari.androidtmdb.theme.AndroidTMDBTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    AppNavigation()
+                    AppNavigation(navController = rememberNavController())
                 }
             }
         }
